309e0eeef6353a441e29a7db81ba8ad79384ea22
a_nuo <gaoluping1985@163.com>
2015-10-28 22:20:26 +0800

    test add

--- js/app.jsx
+++ js/app.jsx
@@ -5,5 +5,6 @@ const App = require("./components/App");
 
 // When the window is loaded, render the App component.
 window.onload = () => {
+	
   ReactDOM.render(<App/>,document.querySelector("#root"));
 }
